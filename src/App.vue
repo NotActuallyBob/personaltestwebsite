c1f6c20b883a63f2de3142fc95dba238a4d65cde
Mikael Snellman <mkael.virkkunen@gmail.com>
2025-01-20 19:07:22 +0200

    Mobile friendly navigation

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const drawer = ref(false);
 </script>
 
 <template>
@@ -10,22 +13,42 @@
         </router-link>
       </v-app-bar-title>
 
-      <router-link to="/about">
-        <v-btn class="mx-2" elevation="2" rounded>
-          About
-        </v-btn>
-      </router-link>
-      <router-link to="/projects">
-        <v-btn class="mr-4 ml-2" elevation="2" rounded>
-          Projects
-        </v-btn>
-      </router-link>
-      <router-link to="/contact">
-        <v-btn class="mr-4 ml-2" elevation="2" rounded>
-          Contact
-        </v-btn>
-      </router-link>
+      <v-app-bar-nav-icon class="d-md-none" @click="drawer = !drawer" />
+
+      <div class="d-none d-md-flex">
+        <router-link to="/about">
+          <v-btn class="mr-2" elevation="2" rounded>
+            About
+          </v-btn>
+        </router-link>
+        <router-link to="/projects">
+          <v-btn class="mr-2" elevation="2" rounded>
+            Projects
+          </v-btn>
+        </router-link>
+        <router-link to="/contact">
+          <v-btn class="mr-2" elevation="2" rounded>
+            Contact
+          </v-btn>
+        </router-link>
+      </div>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list
+        dense
+        nav>
+        <v-list-item prepend-icon="mdi-information">
+          <router-link to="/about">About</router-link>
+        </v-list-item>
+        <v-list-item prepend-icon="mdi-library">
+          <router-link to="/projects">Projects</router-link>
+        </v-list-item>
+        <v-list-item prepend-icon="mdi-email">
+          <router-link to="/contact">Contact</router-link>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <v-container>
@@ -34,7 +57,7 @@
             <RouterView />
           </v-col>
         </v-row>
-      </v-container> 
+      </v-container>
     </v-main>
   </v-app>
 </template>
