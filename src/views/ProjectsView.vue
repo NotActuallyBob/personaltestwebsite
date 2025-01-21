ede560da4419333c6f4df42aa4ac573281e2eb0c
Mikael Snellman <mkael.virkkunen@gmail.com>
2025-01-21 21:41:22 +0200

    add ticket ticker app

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-    import image from "@/assets/projects/asset_atlas.png";
+    import assetAtlasImage from "@/assets/projects/asset_atlas.png";
+    import ticketTickerImage from "@/assets/projects/ticket_ticker.png";
     import ProjectCard from "@/components/ProjectCard.vue";
 </script>
 
@@ -18,7 +19,17 @@
                 <li>Clean expense analysis view to analyze the expenses of the desired time period</li>
             </ul>
         </div>
-        <img class="my-4" width="100%" :src="image">
+        <img class="my-4" width="100%" :src="assetAtlasImage">
+    </ProjectCard>
+
+    <ProjectCard class="my-4"
+        title="Ticket Ticker"
+        url="https://github.com/NotActuallyBob/"
+    >
+        <div>
+            A small mobile application for tracking how many HSL tickets one has purchased. Made using .NET MAUI.
+        </div>
+        <img class="my-4" width="30%" :src="ticketTickerImage">
     </ProjectCard>
 </template>
 
